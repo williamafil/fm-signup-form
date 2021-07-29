a484c2cc27517bc83ab76c2102505f61822cf615
ToroChu <whileonrails@gmail.com>
2021-07-30 02:04:53 +0800

    setup vitawind (tailwind css wit JIT)

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,35 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+    <div className="text-center">
+      <header className="bg-[#282c34] min-h-screen text-white flex flex-col justify-center items-center">
+        <img
+          src={logo}
+          className="h-60 motion-safe:animate-spin animate-speed"
+          alt="logo"
+        />
+        <style>
+          {"\
+            .animate-speed{\
+              animation-duration:20s;\
+            }\
+          "}
+        </style>
+        <p className="text-3xl font-bold">Hello Vite + React!</p>
+        <p className="mt-3">
+          <button
+            type="button"
+            className="rounded text-[#282C34] bg-gray-300 px-2 py-2 my-6 hover:bg-gray-200 transition-all"
+            onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
         </p>
         <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
+          Edit <code className="text-[#8d96a7]">App.jsx</code> and save to test HMR updates.
         </p>
-        <p>
+        <p className="mt-3 flex gap-4 text-center text-[#8d96a7]">
           <a
-            className="App-link"
+            className="text-[#61dafb] hover:text-blue-400 transition-all"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -29,7 +43,7 @@ function App() {
           </a>
           {' | '}
           <a
-            className="App-link"
+            className="text-[#61dafb] hover:text-blue-400 transition-all"
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
